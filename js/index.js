6627897ec719583b3717b8c3ac84e1c97f753f3c
TienLuue <tienluu1301@gmail.comm>
2022-06-09 10:35:16 +0700

    First commit

---
+++ js/index.js
@@ -0,0 +1,69 @@
+/*=== INIT WOW ===*/
+new WOW().init();
+
+/*=== INIT COUNTUP ===*/
+$(".counter").countUp({
+   time: 2000,
+   delay: 10,
+});
+
+/*=== INIT SWIPERJS - FEEDBACK SLIDER===*/
+var imgSlider = new Swiper(".feedback__client", {
+   slidesPerView: 3,
+   spaceBetween: 10,
+   centeredSlides: true,
+   loop: true,
+   speed: 3000,
+   autoplay: true,
+   freeMode: true,
+});
+
+var commentSlider = new Swiper(".feedback__text", {
+   spaceBetween: 10,
+   loop: true,
+   speed: 3000,
+   autoplay: {
+      delay: 3000,
+   },
+   thumbs: {
+      swiper: imgSlider,
+   },
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+});
+
+/*=== INIT SWIPERJS - BLOG SLIDER ===*/
+var blogSlider = new Swiper(".blog__content", {
+   slidesPerView: 3,
+   spaceBetween: 30,
+   loop: true,
+});
+
+/*=== SCROLL TO TOP ===*/
+$(window).scroll(function () {
+   if ($(this).scrollTop() > 50) {
+      $(".scrollToTop").fadeIn();
+   } else {
+      $(".scrollToTop").fadeOut();
+   }
+});
+
+$(".scrollToTop").click(function () {
+   $("html, body").animate({ scrollTop: 0 }, 1000);
+   return false;
+});
+
+/*=== PORTFOLIO GALLERY ===*/
+lightGallery(document.getElementById("animated-thumbnails"), {
+   selector: ".portfolio-img",
+   plugins: [lgZoom, lgThumbnail, lgAutoplay, lgFullscreen, lgShare],
+   alignThumbnails: "left",
+   loop: true,
+   allowMediaOverlap: true,
+   toggleThumb: true,
+   showZoomInOutIcons: true,
+   actualSize: false,
+   exThumbImage: "data-exthumbimage",
+});
